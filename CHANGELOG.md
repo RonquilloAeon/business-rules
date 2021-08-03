b1dacd2a0d738e261ddf6588cd0dcad72c06127e
Martin R <23411718+RonquilloAeon@users.noreply.github.com>
2021-08-03 15:57:38 -0500

    Create CHANGELOG and add long description to setup.py

---
+++ CHANGELOG.md
@@ -0,0 +1,25 @@
+# Changelog
+
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/).
+
+## [2.0.0] - 3 August 2021
+### Added
+- Add Github Actions workflow
+- Add nox for code formatting, linting, and testing
+
+### Changed
+- Made support explicit to Python >=3,<4
+
+### Removed
+- Remove Python 2 support
+- Remove `mock` test dependency
+
+## [1.0.1] - 16 March 2016
+### Fixed
+- Fixed a packaging bug preventing 1.0.0 from being installed on some platforms.
+
+## [1.0.0] - 16 March 2016
+### Changed
+- Remove caching layer on rule decorator
--- setup.py
+++ setup.py
@@ -2,21 +2,23 @@ from setuptools import setup, find_packages
 
 from business_rules.__version__ import __version__ as version
 
-with open("HISTORY.rst") as f:
-    history = f.read()
-
 description = (
     "Python DSL for setting up business intelligence rules that can be "
-    "configured without code"
+    "configured without code. Based on venmo/business-rules."
 )
+
+with open("README.md", "r", encoding="utf-8") as f:
+    readme = f.read()
 
 setup(
     name="business-rules-reloaded",
     version=version,
-    description="{0}\n\n{1}".format(description, history),
     author="RonquilloAeon",
     author_email="23411718+RonquilloAeon@users.noreply.github.com",
     url="https://github.com/RonquilloAeon/business-rules",
+    description=description,
+    long_description=readme,
+    long_description_content_type="text/markdown",
     license="MIT",
     packages=find_packages(),
     python_requires=">=3.7,<4",
